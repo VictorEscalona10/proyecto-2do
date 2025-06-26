8a0c4f8aa78d9165d4025fb667eabeaa0183e5fb
VictorAdmin <victorescalona2006@gmail.com>
2025-06-26 19:00:43 -0400

    avances con los productos

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [AuthModule, 
-    ConfigModule.forRoot({isGlobal: true})
+    ConfigModule.forRoot({isGlobal: true}), ProductsModule
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/products/products.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class ProductsService {
+  constructor(private prisma: PrismaService) {}
+  async getAllProducts() {
+    return this.prisma.product.findMany();
+  }
+}
