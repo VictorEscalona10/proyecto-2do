f01c222326c2a2bd24de02c36ed51c1c18c6872c
VictorAdmin <victorescalona2006@gmail.com>
2025-06-27 12:45:09 -0400

    agrregada las cartegorias y e env.example, hecha la migracion a postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { ProductsModule } from './products/products.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
   imports: [AuthModule, 
-    ConfigModule.forRoot({isGlobal: true}), ProductsModule
+    ConfigModule.forRoot({isGlobal: true}), ProductsModule, CategoryModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,10 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { HttpException } from '@nestjs/common/exceptions/http.exception';
+import { Product } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ProductsService {
   constructor(private prisma: PrismaService) {}
-  async getAllProducts() {
-    return this.prisma.product.findMany();
+
+  async getAll(): Promise<{ message: string; data: Product[] }> {
+    try {
+      const products = await this.prisma.product.findMany();
+      return {
+        message: 'Productos obtenidos correctamente',
+        data: products,
+      };
+    } catch (error) {
+      if (error instanceof HttpException) throw error;
+        throw new InternalServerErrorException('Error al obtener los productos');
+    }
   }
+
+  
 }
