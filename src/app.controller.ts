598f267fe26fcc9a66b077979a09e976a1d19838
Victor Escalona <victorescalona2006@gmail.com>
2025-09-25 11:43:58 -0400

    http codes hechos

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { HttpCode, HttpStatus } from '@nestjs/common';
 
 @Controller()
 export class AppController {
 
   @Get('protected')
   @UseGuards(JwtAuthGuard)
+  @HttpCode(HttpStatus.OK)
   getProtected(): Object {
     return {
       message: 'Acceso concedido',
@@ -15,6 +17,7 @@ export class AppController {
   }
 
   @Get()
+  @HttpCode(HttpStatus.OK)
   hello(){
     return 'API is running...';
   }
