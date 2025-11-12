f790bb41c16df5b03effe063048ee8763a22bd51
Victor Escalona <victorescalona2006@gmail.com>
2025-11-11 20:32:22 -0400

    se agrego un campo a usuario y se modifoco el delete user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Query, Get, Delete, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Query, Get, Delete, HttpCode, HttpStatus, Patch, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersSearchDto } from './dto/usersSearch.dto';
 import { UseGuards } from '@nestjs/common';
@@ -35,5 +35,4 @@ export class UsersController {
   async deleteUser(@Query('email') email: string) {
     return this.usersService.delete(email);
   }
-
 }
