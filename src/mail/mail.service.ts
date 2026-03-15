1b46c405ebc6089fb596391fca7d2a5fea9c1df3
Victor Escalona <victorescalona2006@gmail.com>
2026-03-15 14:17:53 -0400

    vamos a ver si funciona en produccion modifique el transporter de multer

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -10,9 +10,7 @@ export class MailService implements OnModuleInit {
 
   onModuleInit() {
     this.transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST || 'smtp.gmail.com', // Agregamos fallback por si es undefined
-      port: parseInt(process.env.MAIL_PORT || '587'),  // Agregamos fallback dentro del parseInt
-      secure: true,
+      service: 'gmail',
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASSWORD,
